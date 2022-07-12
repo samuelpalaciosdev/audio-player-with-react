e66b40e9d46d8715b90e15ebdc48c5cc455a9ff9
Samuel Palacios <samuelpl0888@gmail.com>
2022-07-11 21:02:23 -0400

    Add initial states and ref

--- src/Components/AudioPlayer.jsx
+++ src/Components/AudioPlayer.jsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 const AudioPlayer = () => {
+  const [songs, setSongs] = useState(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [current, setCurrent] = useState(null);
+
+  let audioRef = useRef(null);
+
+  const setAudioRef = ({ id, src }) => {
+    audioRef.current.src = src;
+    audioRef.current.id = id;
+  };
+
   return (
     <>
       <h1>Audio</h1>
