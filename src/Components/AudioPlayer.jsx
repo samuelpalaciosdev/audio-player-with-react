8e76ff0865f8ab5e6d219ef39200705a644ab014
Samuel Palacios <samuelpl0888@gmail.com>
2022-07-11 21:47:16 -0400

    Fetch data from API

--- src/Components/AudioPlayer.jsx
+++ src/Components/AudioPlayer.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 
+let api = "http://assets.breatheco.de/apis/sound/songs";
+
 const AudioPlayer = () => {
   const [songs, setSongs] = useState(null);
   const [isPlaying, setIsPlaying] = useState(false);
@@ -8,15 +10,79 @@ const AudioPlayer = () => {
   let audioRef = useRef(null);
 
   const setAudioRef = ({ id, src }) => {
-    audioRef.current.src = src;
     audioRef.current.id = id;
+    audioRef.current.src = src;
+  };
+
+  // Run only once
+  useEffect(() => {
+    getSongs(api);
+  }, []);
+
+  // Fetching data from API
+  const getSongs = (url) => {
+    fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((data) => {
+        // Setting data obtained from API to the songs state
+        setSongs(data);
+      })
+      // Handle errors
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   return (
     <>
-      <h1>Audio</h1>
+      <h1>Hey</h1>
     </>
   );
 };
 
 export default AudioPlayer;
+
+// import React, { useState, useRef, useEffect } from "react";
+
+// let api = "http://assets.breatheco.de/apis/sound/songs";
+
+// const AudioPlayer = () => {
+//   const [songs, setSongs] = useState(null);
+//   const [isPlaying, setIsPlaying] = useState(false);
+//   const [current, setCurrent] = useState(null);
+
+//   let audioRef = useRef(null);
+
+//   const setAudioRef = ({ id, src }) => {
+//     audioRef.current.src = src;
+//     audioRef.current.id = id;
+//   };
+
+//   const getSongs = (url) => {
+//     fetch(url, {
+//       method: "GET",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//     })
+//       .then((response) => response.json())
+//       .then((data) => setSongs(data))
+//       .catch((error) => console.log(error));
+//   };
+
+//   return (
+//     <>
+//       <h1>Audio</h1>
+//     </>
+//   );
+// };
+
+// export default AudioPlayer;
